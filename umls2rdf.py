226d961ea218e4d217246cd96b5076f2322456db
Ubuntu <ubuntu@ip-172-31-0-169.us-west-2.compute.internal>
2019-08-18 03:02:52 +0000

    update for python3 compatibility

--- umls2rdf.py
+++ umls2rdf.py
@@ -5,11 +5,12 @@ DEBUG = False
 import codecs
 import sys
 import os
-import urllib
+import urllib.parse as urllib
 from string import Template
 import collections
 import MySQLdb
 import pdb
+from functools import reduce
 #from itertools import groupby
 
 try:
@@ -117,7 +118,7 @@ def get_code(reg,load_on_cuis):
         return reg[MRCONSO_CUI]
     if reg[MRCONSO_CODE]:
         return reg[MRCONSO_CODE]
-    raise AttributeError, "No code on reg [%s]"%("|".join(reg))
+    raise AttributeError( "No code on reg [%s]"%("|".join(reg)))
 
 def __get_connection():
     return MySQLdb.connect(host=conf.DB_HOST,user=conf.DB_USER,
@@ -254,8 +255,8 @@ class UmlsClass(object):
 
     def code(self):
         codes = set([get_code(x,self.load_on_cuis) for x in self.atoms])
-        if len(codes) <> 1:
-            raise AttributeError, "Only one code per term."
+        if len(codes) != 1:
+            raise AttributeError("Only one code per term.")
         #if DEBUG:
             #sys.stderr.write(self.atoms)
             #sys.stderr.write(codes)
@@ -263,7 +264,7 @@ class UmlsClass(object):
 
     def getAltLabels(self,prefLabel):
         #is_pref_atoms =  filter(lambda x: x[MRCONSO_ISPREF] == 'Y', self.atoms)
-        return set([atom[MRCONSO_STR] for atom in self.atoms if atom[MRCONSO_STR] <> prefLabel])
+        return set([atom[MRCONSO_STR] for atom in self.atoms if atom[MRCONSO_STR] != prefLabel])
 
     def getPrefLabel(self):
         if self.load_on_cuis:
@@ -302,7 +303,7 @@ class UmlsClass(object):
                 pref_atom = filter(lambda x: 'P' in x[MRCONSO_TTY], self.atoms)
                 if len(pref_atom) == 1:
                     return pref_atom[0][MRCONSO_STR]
-            raise AttributeError, "Unable to select pref label"
+            raise AttributeError("Unable to select pref label")
 
     def getURLTerm(self,code):
         return get_url_term(self.ns,code)
@@ -312,7 +313,7 @@ class UmlsClass(object):
 
     def toRDF(self,fmt="Turtle",hierarchy=True,lang="en",tree=None):
         if not fmt == "Turtle":
-            raise AttributeError, "Only fmt='Turtle' is currently supported"
+            raise AttributeError("Only fmt='Turtle' is currently supported")
         term_code = self.code()
         url_term = self.getURLTerm(term_code)
         prefLabel = self.getPrefLabel()
@@ -325,16 +326,13 @@ class UmlsClass(object):
         if len(altLabels) > 0:
             rdf_term += """\tskos:altLabel %s ;
 """%(" , ".join(map(lambda x: '\"\"\"%s\"\"\"@%s'%(escape(x),lang),
-                                                            set(altLabels))))
-
+                                                            sorted(set(altLabels)))))
         if self.is_root:
             rdf_term += '\trdfs:subClassOf owl:Thing ;\n'
-
         if len(self.defs) > 0:
             rdf_term += """\tskos:definition %s ;
 """%(" , ".join(map(lambda x: '\"\"\"%s\"\"\"@%s'%(escape(x[MRDEF_DEF]),lang),
                                                                 set(self.defs))))
-
         count_parents = 0
         if tree:
             if term_code in tree:
@@ -344,8 +342,8 @@ class UmlsClass(object):
         for rel in self.rels:
             source_code = get_rel_code_source(rel,self.load_on_cuis)
             target_code = get_rel_code_target(rel,self.load_on_cuis)
-            if source_code <> term_code:
-                raise AttributeError, "Inconsistent code in rel"
+            if source_code != term_code:
+                raise AttributeError("Inconsistent code in rel")
             # Map child relations to rdf:subClassOf (skip parent relations).
             if rel[MRREL_REL] == 'PAR':
                 continue
@@ -370,7 +368,7 @@ class UmlsClass(object):
                 if p not in self.class_properties:
                     self.class_properties[p] = \
                         UmlsAttribute(p,get_rel_fragment(rel))
-
+        
         for att in self.atts:
             atn = att[MRSAT_ATN]
             atv = att[MRSAT_ATV]
@@ -397,11 +395,11 @@ class UmlsClass(object):
 
         #for t in auis:
         #    rdf_term += """\t%s \"\"\"%s\"\"\"^^xsd:string ;\n"""%(HAS_AUI,t)
-        for t in cuis:
+        for t in sorted(cuis):
             rdf_term += """\t%s \"\"\"%s\"\"\"^^xsd:string ;\n"""%(HAS_CUI,t)
-        for t in set(types):
+        for t in sorted(set(types)):
             rdf_term += """\t%s \"\"\"%s\"\"\"^^xsd:string ;\n"""%(HAS_TUI,t)
-        for t in set(types):
+        for t in sorted(set(types)):
             rdf_term += """\t%s <%s> ;\n"""%(HAS_STY,get_umls_url("STY")+t)
 
         return rdf_term + " .\n\n"
@@ -427,14 +425,14 @@ class UmlsAttribute(object):
 
     def toRDF(self,dockey,desc,fmt="Turtle"):
         if not fmt == "Turtle":
-            raise AttributeError, "Only fmt='Turtle' is currently supported"
+            raise AttributeError("Only fmt='Turtle' is currently supported")
         _type = ""
         if "REL" in dockey:
             _type = "ObjectProperty"
         elif dockey == "ATN":
             _type = "DatatypeProperty"
         else:
-            raise AttributeError, ("Unknown DOCKEY" + dockey)
+            raise AttributeError(("Unknown DOCKEY" + dockey))
 
         label = self.att
         if len(desc) < 20:
@@ -442,7 +440,6 @@ class UmlsAttribute(object):
         if "_" in label:
             label = " ".join(self.att.split("_"))
             label = label[0].upper() + label[1:]
-
         return """<%s> a owl:%s ;
 \trdfs:label \"\"\"%s\"\"\";
 \trdfs:comment \"\"\"%s\"\"\" .
@@ -460,6 +457,7 @@ class UmlsOntology(object):
         #self.alt_uri_code = alt_uri_code
         self.atoms = list()
         self.atoms_by_code = collections.defaultdict(lambda : list())
+        #self.atoms_by_code_ls = list() ???
         if not self.load_on_cuis:
             self.atoms_by_aui = collections.defaultdict(lambda : list())
         self.rels = list()
@@ -497,6 +495,7 @@ class UmlsOntology(object):
         for atom in mrconso.scan(filt=mrconso_filt,limit=limit):
             index = len(self.atoms)
             self.atoms_by_code[get_code(atom,self.load_on_cuis)].append(index)
+            #self.atoms_by_code_ls.append(get_code(atom,self.load_on_cuis)) ???
             if not self.load_on_cuis:
                 self.atoms_by_aui[atom[MRCONSO_AUI]].append(index)
             self.atoms.append(atom)
@@ -505,7 +504,6 @@ class UmlsOntology(object):
             sys.stderr.write("length atoms_by_aui: %d\n" % len(self.atoms_by_aui))
             sys.stderr.write("atom example: %s\n\n" % str(self.atoms))
             sys.stderr.flush()
-
         mrconso_filt = "SAB = 'SRC' AND CODE = 'V-%s'"%self.ont_code
         for atom in mrconso.scan(filt=mrconso_filt,limit=limit):
             self.cui_roots.add(atom[MRCONSO_CUI])
@@ -580,7 +578,7 @@ class UmlsOntology(object):
         for code in self.atoms_by_code:
             code_atoms = [self.atoms[row] for row in self.atoms_by_code[code]]
             field = MRCONSO_CUI if self.load_on_cuis else MRCONSO_AUI
-            ids = map(lambda x: x[field], code_atoms)
+            ids = list(map(lambda x: x[field], code_atoms))
             rels = list()
             for _id in ids:
                 rels += [self.rels[x] for x in self.rels_by_aui_src[_id]]
@@ -609,10 +607,10 @@ class UmlsOntology(object):
                         if rel[MRREL_CUI1] == "C3264380" and rel[MRREL_REL] == "CHD":
                             is_root = True
 
-                    if len(code_source) <> 1 or len(code_target) > 1:
-                        raise AttributeError, "more than one or none codes"
+                    if len(code_source) != 1 or len(code_target) > 1:
+                        raise AttributeError("more than one or none codes")
                     if len(code_source) == 1 and len(code_target) == 1 and \
-                        code_source[0] <> code_target[0]:
+                        code_source[0] != code_target[0]:
                         code_source = code_source[0]
                         code_target = code_target[0]
                         # NOTE: the order of these append operations below is important.
@@ -626,7 +624,6 @@ class UmlsOntology(object):
             aui_codes_def = [item for sublist in aui_codes_def for item in sublist]
             defs = [self.defs[index] for index in aui_codes_def]
             atts = [self.atts[x] for x in self.atts_by_code[code]]
-
             umls_class = UmlsClass(self.ns,atoms=code_atoms,rels=rels_to_class,
                 defs=defs,atts=atts,rank=self.rank,rank_by_tty=self.rank_by_tty,
                 sty=self.sty, sty_by_cui=self.sty_by_cui,
@@ -661,13 +658,11 @@ class UmlsOntology(object):
             try:
                 rdf_text = term.toRDF(lang=UMLS_LANGCODE_MAP[self.lang],tree=self.tree)
                 fout.write(rdf_text)
-            except Exception, e:
-                print "ERROR dumping term ", e
-
+            except Exception as e:
+                print("ERROR dumping term ", e)
             for att in term.properties():
                 if att not in self.ont_properties:
                     self.ont_properties[att] = term.properties()[att]
-
         return fout
 
     def properties(self):
@@ -686,7 +681,7 @@ class UmlsOntology(object):
                 continue
             doc = property_docs[prop.att]
             if "expanded_form" not in doc:
-                raise AttributeError, "expanded form not found in " + doc
+                raise AttributeError("expanded form not found in " + doc)
             _desc = doc["expanded_form"]
             if "inverse" in doc:
                 _desc = "Inverse of " + doc["inverse"]
@@ -756,7 +751,7 @@ if __name__ == "__main__":
         ns = get_umls_url(umls_code if not alt_uri_code else alt_uri_code)
         ont = UmlsOntology(umls_code,ns,con,load_on_cuis=load_on_cuis)
         ont.load_tables()
-        fout = ont.write_into(output_file,hierarchy=(ont.ont_code <> "MSH"))
+        fout = ont.write_into(output_file,hierarchy=(ont.ont_code != "MSH"))
         ont.write_properties(fout,property_docs)
         if conf.INCLUDE_SEMANTIC_TYPES:
           ont.write_semantic_types(sem_types,fout)
